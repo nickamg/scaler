ebe22f632df78784722210fd1ad51aacd1593132
nmagallon <nicolasmagallon.design@gmail.com>
2018-06-11 22:28:27 +0200

    Código refactorizado y añadida funcionalidad de no repetir notas al calcular las notas de la escala

--- js/scale.js
+++ js/scale.js
@@ -1,88 +1,81 @@
 var scaleName;
 var tonicName;
 
+// SCALE OBJECT
+
 function Scale(name, degrees, tonic) {
     this.name = name;
     this.degrees = degrees;
-    this.notes = addNotes(tonic, degrees);
+    this.notes = getNotes(tonic, degrees);
 
-    /*
-    las escalas empiezan por la tónica y siguen para adelante
-    Las escalas tienen que tener todas las notas (nombres no repetidos)
-    Las escalas que tienen en la tónica un sostenido tienen todo sostenidos y viceversa
-    Las escalas que tienen a partir 7 (no incluido) alteraciones invierten la tónica
-    Los dobles sostenidos cuentan como dos sostenidos
-    Cuando la tónica se invierte mostrar mensaje "tal vez quiso decir..."
-    */
-
-    function addNotes(tonic, degrees) {
+    function getNotes(tonic, degrees) {
         var noteNames = [["c", "b#"], ["c#", "db"], ["d", "c##"], ["d#", "eb"], ["e", "fb", "d##"], ["f", "e#"], ["f#", "gb", "e##"], ["g", "f##"], ["g#", "ab"], ["a", "g##"], ["a#", "bb"], ["b", "cb", "a##"]];
-        var resultNotes = [];
-        for(var i = 0; i < degrees.length; i++) {
-            resultNotes.push(findNoteByDegree(noteNames, tonic, degrees[i].degree_position));
-        }
-        checkNotes(tonic, noteNames, resultNotes);
-        return resultNotes;
-    }
-
-    function checkNotes(tonic, noteNames, resultNotes) {
         var naturalNotes = ["c", "d", "e", "f", "g", "a", "b"];
-        var naturalTonic = findNaturalTonic(tonic, naturalNotes);
-        for(var i = 1; i < resultNotes.length; i++) {
-            if(resultNotes[i].charAt(0) != naturalNotes[nextNote(naturalNotes, naturalTonic, i)]) {
-                for(var j = 0; j < noteNames[findNote(resultNotes[i], noteNames)].length; j++) {
-                    if(noteNames[findNote(resultNotes[i], noteNames)][j].charAt(0) == naturalNotes[nextNote(naturalNotes, naturalTonic, i)]) {
-                        resultNotes[i] = noteNames[findNote(resultNotes[i], noteNames)][j];
+        var findTonic = (function() {
+            for(var i = 0; i < noteNames.length; i++) {
+                for(var j = 0; j < noteNames[i].length; j++) {
+                    if(tonic == noteNames[i][j]) {
+                        return [i, j];
                     }
                 }
             }
-        }
-    }
-
-    function nextNote(naturalNotes, naturalTonic, interval) {
-        if(naturalTonic + interval > naturalNotes.length - 1) {
-            return (naturalTonic + interval) - naturalNotes.length;
-        } else {
-            return naturalTonic + interval;
-            alert("menor que");
-        }
-    }
-
-    function findNaturalTonic(tonic, naturalNotes) {
-        for(var i = 0; i < naturalNotes.length; i++) {
-            if(tonic.charAt(0) == naturalNotes[i]) {
-                return i;
-            }
-        }
-        return null;
-    }
-
-    /*
-    Encuentra sólo la primera, es decir, da problemas al poner por ejemplo e#, ya que escribirá f.
-    */
-    function findNoteByDegree(noteNames, tonic, interval) {
-        var note = findNote(tonic, noteNames);
-        for(var j = 0; j < interval - 1; j++) {
-            if(note == noteNames.length - 1) {
-                note = 0;
-            } else {
-                note++;
-            }
-        }
-        return noteNames[note][0];
-    }
-    
-    function findNote(note, noteNames) {
-        for(var i = 0; i < noteNames.length; i++) {
-            for(var j = 0; j < noteNames[i].length; j++) {
-                if(note == noteNames[i][j]) {
+            return null;
+        });
+        var findNaturalTonic = (function() {
+            for(var i = 0; i < naturalNotes.length; i++) {
+                if(tonic.charAt(0) == naturalNotes[i]) {
                     return i;
                 }
             }
+            return null;
+        });
+        var nextNaturalNote = (function(interval) {
+            var newPosition = naturalTonicPosition;
+            //alert(interval + " es el intervalo de la nota natural")
+            for(var i = 0; i < interval; i++) {
+                if(newPosition == naturalNotes.length - 1) {
+                    newPosition = 0;
+                } else {
+                    newPosition++;
+                }
+            }
+            return newPosition;
+        });
+        var nextNotePosition = (function(interval) {
+            var newPosition = tonicPosition[0];
+            //alert(interval + " es el intervalo de la siguiente nota");
+            for(var i = 0; i < interval - 1; i++) {
+                if(newPosition == noteNames.length - 1) {
+                    newPosition = 0;
+                } else {
+                    newPosition++;
+                }
+            }
+            return newPosition;
+        });
+        var getNote = (function(interval) {
+            var notePosition = nextNotePosition(degrees[interval].degree_position);
+            //alert(degrees[interval].degree_position + " es el intervalo dictado por el grado");
+            for(var i = 0; i < noteNames[notePosition].length; i++) {
+                //alert(naturalNotes[nextNaturalNote(interval)] + " es la siguiente nota natural");
+                if(noteNames[notePosition][i].charAt(0) == naturalNotes[nextNaturalNote(interval)]) {
+                    //alert("Entra en el if");
+                    return noteNames[notePosition][i];
+                }
+            }
+        });
+        var tonicPosition = findTonic();
+        var naturalTonicPosition = findNaturalTonic();
+        var resultNotes = [];
+
+        for(var i = 0; i < degrees.length; i++) {
+            resultNotes.push(getNote(i));
         }
-        return null;
+        return resultNotes;
     }
 }
+
+// FUNCTIONS
 
 function setGlobals() {
     scaleName = $("#txtScale").val();
